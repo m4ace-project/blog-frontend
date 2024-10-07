613a9585e9337de9048a8710618996d41614f3e6
Habeeb <ogundelehabeeb01@gmail.com>
2024-10-07 21:13:21 +0100

    Responsive Google sign-in page

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    
        <App />
-    </BrowserRouter>
+       
   </StrictMode>,
 )
