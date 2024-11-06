1109c2e294374e8961b2cf005833b334bd2a2204
Habeeb <ogundelehabeeb01@gmail.com>
2024-11-06 17:32:37 +0100

    update the create post page

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -4,12 +4,16 @@ import PostHeader from '../components/pages/post/PostHeader';
 
 function CreatePost() {
   return (
-    <div className='bg-[#FFFCD8] h-[100vh] pt-5'>
-      <div className='w-[65rem] float-end'>
-        <div className='flex gap-[5rem]'>
-          <div className='w-[26rem]'>
-            <h5 className='bg-white h-[8rem] rounded-[1rem] flex justify-center items-center'>TEXTS</h5>
-            <div className='flex gap-[8rem] my-8 justify-center'>
+    <div className='bg-[#FFFCD8] h-[100vh] md:flex'>
+      <div className='md:w-[20%]'>
+          <Widget />
+      </div>
+      <div className='md:w-[80%]'>
+        <PostHeader />
+        <div className='mx-14 lg:flex lg:gap-[5rem] mt-5 lg:mx-10'>
+          <div className='lg:w-[45%] mb-14 md:mb-0'>
+            <h5 className='bg-white h-[10rem] w-[70%] place-self-center lg:w-full lg:h-[7rem] rounded-[1rem] flex justify-center items-center'>TEXTS</h5>
+            <div className='flex gap-[8rem] my-10 lg:my-5 justify-center'>
               <div className='flex gap-5'>
                 <img src="./src/assets/image.svg" alt="" />
                 <img src="./src/assets/lucide_video.svg" alt="" />
@@ -18,21 +22,21 @@ function CreatePost() {
               <button className='bg-[#FF5722] text-white w-[9rem] h-[2rem] rounded-xl'>Publish</button>
             </div>
             <div>
-            <h5 className='text-[#001F54] text-xl font-bold text-center mb-7'>Recent Posts</h5>
-            <div className='flex gap-7'>
-              <div>
-                <img src="./src/assets/Rectangle 345.svg" className='h-[12rem]' alt="" />
+            <h5 className='text-[#001F54] text-xl font-bold text-center mb-10 lg:mb-4'>Recent Posts</h5>
+            <div className='md:w-[100%] md:flex md:justify-between md:gap-5 ml-5 '>
+              <div className='sm:w-[50%] md:w-[45%] mb-14 mx-auto lg:w-[50%]'>
+                <img src="./src/assets/Rectangle 345.svg" className='lg:h-[60%]' alt="" />
                 <p className='my-3 text-[#001F54] font-bold'>How to Style Long Shorts for Work this Summer</p>
               </div>
-              <div>
-                <img src="./src/assets/Rectangle 346.svg" className='h-[12rem]' alt="" />
+              <div className='sm:w-[50%] md:w-[45%] mx-auto lg:w-[50%]'>
+                <img src="./src/assets/Rectangle 346.svg" className='lg:h-[50%]' alt="" />
                 <p className='my-3 text-[#001F54] font-bold'>Naira Depreciates to N1,595$ in Parallel Market</p>
               </div>
             </div>
           </div>
           </div>
-          <div className='w-[24rem]'>
-            <div className='bg-white w-[20rem] h-[6rem] rounded-[1rem] mb-8 p-3'>
+          <div className='ml-10 lg:w-[45%]'>
+            <div className='bg-white md:w-[72%] h-[6rem] rounded-[1rem] mb-14 lg:mb-4 p-3'>
               <div className='flex gap-4 mb-2'>
                 <button className='bg-[#FF5722] text-white w-[8rem] h-[2rem] rounded-xl'>Last 7 Days</button>
                 <p>Last 30 Days</p>
@@ -45,13 +49,13 @@ function CreatePost() {
               </div>
             </div>
             <div className='text-[#001F54]'>
-              <h5 className='font-bold text-center my-3'>Top Posts</h5>
+              <h5 className='font-bold text-center mb-8 text-3xl lg:text-lg lg:mb-3'>Top Posts</h5>
               <p>Drama as popular comedian, Osama berates Rhythm FM & OAP David King on live Radio</p>
-              <p className='my-3'>You Have to See Nini Singh’s Style Delivery at London Fashion Week</p>
+              <p className='my-6 lg:my-2'>You Have to See Nini Singh’s Style Delivery at London Fashion Week</p>
               <p>FG Unveils Schedule For 2024 Independence Day Celebration</p>
-              <p className='my-3'>#BNxBBNaija9: Handi, Shaun & Tjay Evicted in Triple Exit | Highlights You Missed</p>
+              <p className='my-6 lg:my-2'>#BNxBBNaija9: Handi, Shaun & Tjay Evicted in Triple Exit | Highlights You Missed</p>
               <p>HerMoneyHerPower: Funke Akindele Shares How “Jenifa’s Diary” Came To Life by a Woman’s Support</p>
-              <p className='my-3'>Crypto Market Making : Basics That You Need to Know</p>
+              <p className='my-6 lg:my-2'>Crypto Market Making : Basics That You Need to Know</p>
               <p>How to Achieve the Quiet Luxury Trend … </p>
             </div>
           </div>
