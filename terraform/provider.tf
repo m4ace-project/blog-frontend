d237c03157715a1b46f3eddfc6ca1845e06afac7
So-fire <sophiaudofiah@gmail.com>
2024-12-19 09:16:51 -0600

    deploying for Habeeb use

---
+++ terraform/provider.tf
@@ -0,0 +1,5 @@
+# Configure the AWS Provider
+provider "aws" {
+  region  = "us-east-1"         # Replace with your desired AWS region
+  profile = "default"           # Replace with your AWS CLI profile name, if needed
+}
