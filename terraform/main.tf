5097a06c4c616ad76b5d74602598bc678e0fe71e
So-fire <sophiaudofiah@gmail.com>
2025-07-15 14:26:47 +0100

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -79,7 +79,7 @@ resource "aws_s3_bucket_website_configuration" "bucket_hosting_config" {
     # The filemd5() function is available in Terraform 0.11.12 and later
     # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
     # etag = "${md5(file("path/to/file"))}"
-   # etag = each.value.digests.md5.. ..
+   # etag = each.value.digests.md5.. ....
 #}
 
 
